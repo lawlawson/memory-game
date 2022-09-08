68627186fda9da206acf87c875b59e657e0da031
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-09-08 16:59:11 +0100

    Added images and mapped through

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import { useState } from 'react';
 import './App.css';
 
 const cardImages = [
-  { src: 'img/helmet.png' },
-  { src: 'img/potion.png' },
-  { src: 'img/ring.png' },
-  { src: 'img/scroll.png' },
-  { src: 'img/shield.png' },
-  { src: 'img/sword.png' },
+  { src: '/img/helmet-1.png' },
+  { src: '/img/potion-1.png' },
+  { src: '/img/ring-1.png' },
+  { src: '/img/scroll-1.png' },
+  { src: '/img/shield-1.png' },
+  { src: '/img/sword-1.png' },
 ];
 
 function App() {
@@ -29,6 +29,16 @@ function App() {
     <div className='App'>
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+      <div className='card-grid'>
+        {cards.map((card) => (
+          <div className='card' key={card.id}>
+            <div>
+              <img className='front' src={card.src} alt='card front' />
+              <img className='back' src='/img/cover.png' alt='card back' />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
