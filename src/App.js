655203f2a260fd8bdf915714a2b79f417332acaf
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-11-09 23:54:36 +0000

    Added prop for matched card state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import SingleCard from './components/SingleCard';
 import './App.css';
 
 const cardImages = [
-  { src: '/img/helmet-1.png' },
-  { src: '/img/potion-1.png' },
-  { src: '/img/ring-1.png' },
-  { src: '/img/scroll-1.png' },
-  { src: '/img/shield-1.png' },
-  { src: '/img/sword-1.png' },
+  { src: '/img/helmet-1.png', matched: false },
+  { src: '/img/potion-1.png', matched: false},
+  { src: '/img/ring-1.png', matched: false },
+  { src: '/img/scroll-1.png', matched: false },
+  { src: '/img/shield-1.png', matched: false},
+  { src: '/img/sword-1.png', matched: false },
 ];
 
 function App() {
@@ -33,10 +33,10 @@ function App() {
   useEffect(() => {
     if (choiceOne && choiceTwo) {
       if (choiceOne.src === choiceTwo.src) {
-        console.log('Cards match');
+
         resetTurn();
       } else {
-        console.log('Those choices do not match');
+
       }
     }
   }, [choiceOne, choiceTwo]);
